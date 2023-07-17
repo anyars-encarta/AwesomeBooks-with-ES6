3289764d392d6e2afdfd1dfeca7703981963984c
anyars-encarta <anyarsencarta@gmail.com>
2023-07-17 17:00:47 +0000

    Correct import module errors and eslint errors

--- modules/date-time.js
+++ modules/date-time.js
@@ -2,7 +2,7 @@ import { DateTime } from './luxon.js';
 
 const myDateTime = () => {
   const myDocument = document.getElementById('date-time');
- 
+
   const dateTimeUpdate = () => {
     const dateTimeCurrent = DateTime.now().toLocaleString(
       DateTime.DATETIME_FULL_WITH_SECONDS,
