163a256fe7183a2e0cc825c6bdd1be12ab91d23c
anyars-encarta <anyarsencarta@gmail.com>
2023-07-17 17:09:11 +0000

    Update index.js file

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 import bookLoad from './modules/bookBlock.js';
 import myDateTime from './modules/date-time.js';
 import displayNavigation from './modules/displayNavigation.js';
-// import clicks from './modules/clicks.js';
 
 // Fetched from module
 document.addEventListener('DOMContentLoaded', () => {
   bookLoad();
-  // clicks();
   displayNavigation();
   myDateTime();
 });
